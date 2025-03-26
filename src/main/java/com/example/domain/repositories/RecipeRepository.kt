b7d1dc334cfcb446973710615a3942587ce7058a
muradhani <mouradhany574@gmail.com>
2025-03-26 13:58:08 +0200

    Implement API response handling and repository methods

--- src/main/java/com/example/domain/repositories/RecipeRepository.kt
+++ src/main/java/com/example/domain/repositories/RecipeRepository.kt
@@ -3,6 +3,6 @@ package com.example.domain.repositories
 import com.example.domain.models.RecipeEntity
 
 interface RecipeRepository {
-    suspend fun search(token: String, page: Int, query: String): List<RecipeEntity>
-    suspend fun get(token:String , id:Int):RecipeEntity
+    suspend fun search(token: String, page: Int, query: String): List<RecipeEntity>?
+    suspend fun get(token:String , id:Int):RecipeEntity?
 }
